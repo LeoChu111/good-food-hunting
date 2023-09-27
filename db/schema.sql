9934f3bbc9fc1d0b812f858e6b23812fed50e1a2
Wen-Hao Chu <leochu111@gmail.com>
2023-09-27 12:51:34 +1000

    update

--- db/schema.sql
+++ db/schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE dishes (
 );
 ALTER TABLE dishes ADD COLUMN user_id INTEGER NOT NULL;
 update dishes set user_id = 1;
-INSERT INTO dishes (title, image_url) 
-VALUES ('cake', 'https://stylesweet.com/wp-content/uploads/2022/06/ChocolateCakeForTwo_Featured.jpg'),
-('pasta', 'https://www.allrecipes.com/thmb/5SdUVhHTMs-rta5sOblJESXThEE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11691-tomato-and-garlic-pasta-ddmfs-3x4-1-bf607984a23541f4ad936b33b22c9074.jpg');
+INSERT INTO dishes (title, image_url, user_id) 
+VALUES ('cake', 'https://stylesweet.com/wp-content/uploads/2022/06/ChocolateCakeForTwo_Featured.jpg', 1),
+('pasta', 'https://www.allrecipes.com/thmb/5SdUVhHTMs-rta5sOblJESXThEE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11691-tomato-and-garlic-pasta-ddmfs-3x4-1-bf607984a23541f4ad936b33b22c9074.jpg',1);
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
